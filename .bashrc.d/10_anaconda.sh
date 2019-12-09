f255140303cc1bf03facf8d510e6828e24f3cfe8
Jonathan Saylor <Jonathan Saylor>
2019-12-08 22:05:06 -0800

    Added safety check on anaconda to check if it is installed

--- .bashrc.d/10_anaconda.sh
+++ .bashrc.d/10_anaconda.sh
@@ -2,8 +2,10 @@
 
 export ANACONDA_HOME="${HOME}/anaconda3"
 
-. ${ANACONDA_HOME}/etc/profile.d/conda.sh
+if [ ! -d $ANACONDA_HOME ]; then
+  . ${ANACONDA_HOME}/etc/profile.d/conda.sh
 
-conda activate
+  conda activate
 
-[ ! -z "$DISPLAY" ] && echo_success "Custom python configuration loaded."
+  [ ! -z "$DISPLAY" ] && echo_success "Custom python configuration loaded."
+fi
