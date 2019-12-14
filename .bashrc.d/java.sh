7eade1e60a52fed24a69c54bfd9a250ac34c2b34
Jonathan Saylor <jonathan.saylor@raytheon.com>
2019-12-13 20:05:40 -0800

    Added java setup
    Conditional if JAVA_DEV is set to anything

---
+++ .bashrc.d/java.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! -z "${JAVA_DEV}" ]; then
+  # Choose the version of java to use.
+  export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
+  export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+  export PATH="${PATH}:${JAVA_HOME}/bin"
+
+  alias intellij="intellij-idea-community &> /dev/null &"
+
+  [ ! -z "$DISPLAY" ] && echo_success "JAVA environment set"
+}
+else {
+  [ ! -z "$DISPLAY" ] && echo_warn "JAVA setup bypassed"
+}
