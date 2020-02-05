f7f10d81e3bce7768ae89ab6a178615d0620f3ba
Jonathan Saylor <jsaylor525@gmail.com>
2020-02-05 11:07:04 -0800

    Changed git prompt to/bash-git-prompt
    * This is a github repo but I haven't linked it in, if it's not cloned the script will print out how to clone it correctly.
    * In the future this maybe a submodule or subtree

--- .bashrc.d/10_git.sh
+++ .bashrc.d/10_git.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
+if [ ! -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
+    echo "You need to install the bash-git-prompt!"
+    echo "Use the following command to get bash-git-prompt"
+    echo ""
+    echo "git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1"
+    echo ""
+else
+    GIT_PROMPT_ONLY_IN_REPO=1
+    # GIT_PROMPT_FETCH_REMOTE_STATUS=0   # uncomment to avoid fetching remote status
+    # GIT_PROMPT_IGNORE_SUBMODULES=1 # uncomment to avoid searching for changed files in submodules
+    # GIT_PROMPT_WITH_VIRTUAL_ENV=0 # uncomment to avoid setting virtual environment infos for node/python/conda environments
 
-function color_my_prompt {
-    local __user_and_host="\[\033[01;32m\]\u@\h"
-    local __cur_location="\[\033[01;34m\]\w"
-    local __git_branch_color="\[\033[31m\]"
-    local __git_branch='`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)/`'
-    local __prompt_tail="\[\033[35m\]$"
-    local __last_color="\[\033[00m\]"
-    export PS1="$__user_and_host:$__cur_location$__git_branch_color$__git_branch$__prompt_tail$__last_color "
-}
-color_my_prompt
+    # GIT_PROMPT_SHOW_UPSTREAM=1 # uncomment to show upstream tracking branch
+    # GIT_PROMPT_SHOW_UNTRACKED_FILES=normal # can be no, normal or all; determines counting of untracked files
+
+    # GIT_PROMPT_SHOW_CHANGED_FILES_COUNT=0 # uncomment to avoid printing the number of changed files
+
+    # GIT_PROMPT_STATUS_COMMAND=gitstatus_pre-1.7.10.sh # uncomment to support Git older than 1.7.10
+
+    # GIT_PROMPT_START=...    # uncomment for custom prompt start sequence
+    # GIT_PROMPT_END=...      # uncomment for custom prompt end sequence
+
+    # as last entry source the gitprompt script
+    # GIT_PROMPT_THEME=Custom # use custom theme specified in file GIT_PROMPT_THEME_FILE (default ~/.git-prompt-colors.sh)
+    # GIT_PROMPT_THEME_FILE=~/.git-prompt-colors.sh
+    # GIT_PROMPT_THEME=Solarized # use theme optimized for solarized color scheme
+    source $HOME/.bash-git-prompt/gitprompt.sh
+fi
 
 # git config --global push.default simple
 git config --global push.default tracking
