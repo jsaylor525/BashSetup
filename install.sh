5214e2e0d7305dd4c8ae38db256ddbda192414d3
Jonathan Saylor <jonathan.saylor@raytheon.com>
2020-02-06 09:27:15 -0800

    Added backup function

--- install.sh
+++ install.sh
@@ -1,22 +1,37 @@
 #/bin/bash
 
+. .bashrc.d/00_colors.sh
+
 # Uncommnet this line to test this in the /tmp/ directory
 # TESTING=1
+timestamp=$(date --iso-8601=seconds)
 
 # Set parameters differently if we're testing the install script.
 if [[ -z "${TESTING}" ]]; then
   BASHRC_FILE="${HOME}/.bashrc"
   BASHRC_DIR="${HOME}/.bashrc.d"
+  BACKUP_DIR="${HOME}/.bashrc_backup_${timestamp}"
   mkdir -p ${BASHRC_DIR}/
 else
   BASHRC_FILE="/tmp/.bashrc"
   BASHRC_DIR="/tmp/.bashrc.d"
+  BACKUP_DIR="/tmp/.bashrc_backup_${timestamp}"
   mkdir -p ${BASHRC_DIR}/
 fi
 
 function test_log() {
-  [ ! -z ${TESTING:+0} ] && echo "[TESTING] $*"
+  [ ! -z ${TESTING:+0} ] && printf "${Yellow}[TESTING] $*${ResetColor}\n"
 }
+
+
+function create_bachrc_backup() {
+  mkdir -p ${BACKUP_DIR}
+  cp -r ${BASHRC_DIR} ${BACKUP_DIR}
+  cp ${BASHRC_FILE} ${BACKUP_DIR}
+  test_log "Created backup: ${BACKUP_DIR}"
+  test_log "contents: $(ls ${BACKUP_DIR})"
+}
+
 
 function add_hook_to_bashrc() {
   check_for_hook=$(cat ${BASHRC_FILE} | grep .bashrc.d)
@@ -25,6 +40,7 @@ function add_hook_to_bashrc() {
     unset cmd
   fi
 }
+
 
 function copy_scripts_to_bashrcd() {
   if [[ -d ".bashrc.d" ]]; then
@@ -40,8 +56,8 @@ function copy_scripts_to_bashrcd() {
   fi
 }
 
+create_bachrc_backup
 add_hook_to_bashrc
-
 copy_scripts_to_bashrcd
 
 if [[ ! -z "${TESTING}" ]]; then
