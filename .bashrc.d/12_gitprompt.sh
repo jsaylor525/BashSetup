a1eaca0592403f95674954fd3ef2067d03a75d94
Jonathan Saylor <jsaylor525@users.noreply.github.com>
2020-02-19 10:37:39 -0600

    Update 12_gitprompt.sh
    * Added back the basic git prompt from earlier, which simply adds color and branch name.
    * Added variable to select 
    
    TODO - could use colors in the basic prompt

--- .bashrc.d/12_gitprompt.sh
+++ .bashrc.d/12_gitprompt.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
 
-# =================== Git Prompt ===================
-function git_prompt() {
+# Variable that defines which prompt to use, options:
+#   - basic
+#   - bash-git-prompt
+GITPROMPT=${GITPROMPT:-"bash-git-prompt"}
+
+
+# ================ Basic Git Prompt ================
+function basic_git_prompt {	    # GIT_PROMPT_STATUS_COMMAND=gitstatus_pre-1.7.10.sh # uncomment to support Git older than 1.7.10
+    local __user_and_host="\[\033[01;32m\]\u@\h"	
+    local __cur_location="\[\033[01;34m\]\w"	    # GIT_PROMPT_START=...    # uncomment for custom prompt start sequence
+    local __git_branch_color="\[\033[31m\]"	    # GIT_PROMPT_END=...      # uncomment for custom prompt end sequence
+    local __git_branch='`git branch 2> /dev/null | grep -e ^* | sed -E  s/^\\\\\*\ \(.+\)$/\(\\\\\1\)/`'	
+    local __prompt_tail="\[\033[35m\]$"	    # as last entry source the gitprompt script
+    local __last_color="\[\033[00m\]"	    # GIT_PROMPT_THEME=Custom # use custom theme specified in file GIT_PROMPT_THEME_FILE (default ~/.git-prompt-colors.sh)
+    export PS1="$__user_and_host:$__cur_location$__git_branch_color$__git_branch$__prompt_tail$__last_color "	    # GIT_PROMPT_THEME_FILE=~/.git-prompt-colors.sh
+}	    # GIT_PROMPT_THEME=Solarized # use theme optimized for solarized color scheme
+
+
+# ================ Bash Git Prompt =================
+function bash_git_prompt() {
     if [ ! -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
         echo "You need to install the bash-git-prompt!"
         echo "Use the following command to get bash-git-prompt"
@@ -32,6 +50,18 @@ function git_prompt() {
         source $HOME/.bash-git-prompt/gitprompt.sh
     fi
 }
-git_prompt
 
-[ ! -z "$DISPLAY" ] && log_success "Git prompt loaded"
+
+case ${GITPROMPT} in
+    basic )
+        basic_git_prompt        
+        [ ! -z "$DISPLAY" ] && log_success "Git prompt loaded"
+        ;;
+    bash-git-prompt )
+        bash_git_prompt        
+        [ ! -z "$DISPLAY" ] && log_success "Git prompt loaded"
+        ;;
+    * )
+        [ ! -z "$DISPLAY" ] && log_error "No git prompt loaded"
+        ;;
+esac
