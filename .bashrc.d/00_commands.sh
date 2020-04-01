c02c4fd5f6b48e7360baaf19993c36933c0a18e1
Jonathan Saylor <Jonathan.Saylor@Raytheon.com>
2020-04-01 16:00:20 -0500

    Renamed basename filename extension and dirname
    This cause issues if they already existed! Which is most systems now.

--- .bashrc.d/00_commands.sh
+++ .bashrc.d/00_commands.sh
@@ -7,43 +7,43 @@ alias network-reset="sudo systemctl restart network-manager.service"
 
 
 # Uses bash string expansion to get the filename
-function dirname() {
+function js_dirname() {
     for d in $@; do
         local directory=${d%*/*.sh}
         echo $directory
     done
 }
-export -f dirname
+export -f js_dirname
 
 
 # Uses bash string expansion to get the filename
-function filename() {
+function js_filename() {
     for f in $@; do
         local fname=${f##*/}
         echo $fname
     done
 }
-export -f filename
+export -f js_filename
 
 
 # Uses bash string expansion to get the extension of file(s)
-function extension() {
+function js_extension() {
     for e in $@; do
         local ext=${e##*.}
         echo $ext
     done
 }
-export -f extension
+export -f js_extension
 
 
 # Uses bash string expansion to get the basename of a file(s) ie. filename without extension
-function basename() {
+function js_basename() {
     for b in $@; do
         local fname=$(filename $b)
         echo ${fname%%.*}
     done
 }
-export -f basename
+export -f js_basename
 
 
 # Make the copy command make a numbered backup copy
